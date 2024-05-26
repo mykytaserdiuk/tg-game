52d1dbcf5964e94f3c27f63cd0bc6756eabc9c11
Mykyta <hiban.com@gmail.com>
2024-05-26 22:55:27 +0200

    v1

--- cmd/soap/main.go
+++ cmd/soap/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -11,17 +14,35 @@ var (
 	coins = make(map[string]int)
 )
 
+func fixContentType(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println(r.RequestURI)
+		w.Header().Add("Content-Type", "application/json")
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	r := mux.NewRouter()
+	r.Use(fixContentType)
 	r.HandleFunc("/coin", AddCoin).Methods(http.MethodPut)
 	r.HandleFunc("/coin", GetCoin).Methods(http.MethodGet)
-	err := http.ListenAndServe(":1234", r)
-	log.Fatal(err)
+	r.HandleFunc("/", Main).Methods(http.MethodGet)
+	log.Println("Server starting....")
+	fmt.Print("FMT")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, r))
+	log.Println("Server start")
 }
 
 func AddCoin(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["user_id"]
+	id := r.URL.Query().Get("user_id")
 	if id == "" {
 		w.WriteHeader(400)
 		return
@@ -31,11 +52,18 @@ func AddCoin(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(string(newC)))
 }
 func GetCoin(w http.ResponseWriter, r *http.Request) {
-	id := mux.Vars(r)["user_id"]
+	log.Print("GET COIN")
+	id := r.URL.Query().Get("user_id")
 	if id == "" {
 		w.WriteHeader(400)
 		return
 	}
 	newC := coins[id]
 	w.Write([]byte(string(newC)))
+}
+func Main(w http.ResponseWriter, r *http.Request) {
+	log.Print("Main, full data")
+
+	data, _ := json.Marshal(&coins)
+	w.Write(data)
 }

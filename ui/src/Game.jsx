009d1b28f0534032d9d4e99810647c93b3dee126
Mykyta <hiban.com@gmail.com>
2024-05-27 00:29:13 +0200

    1

--- ui/src/Game.jsx
+++ ui/src/Game.jsx
@@ -3,26 +3,26 @@ import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 
-const [searchParams] = useSearchParams();
-const [coin, setCoin] = useState(0);
-
-function userId(){
-  return searchParams.get("user_id")
-} 
-
-function getCoins() {
-  axios.get(`tg-game-production-8e6f.up.railway.app/?user_id=${userId()}`)
-  .then(response=> {
-    setCoin(response.data)
-  })
-  .catch(res=>console.log(res))
-}
-function click() {
-  axios.put(`tg-game-production-8e6f.up.railway.app/?user_id=${userId()}`)
-  .catch(res=>console.log(res))
-}
 
 function Game() {
+    const [searchParams] = useSearchParams();
+    const [coin, setCoin] = useState(0);
+    
+    function userId(){
+      return searchParams.get("user_id")
+    } 
+    
+    function getCoins() {
+      axios.get(`tg-game-production-8e6f.up.railway.app/?user_id=${userId()}`)
+      .then(response=> {
+        setCoin(response.data)
+      })
+      .catch(res=>console.log(res))
+    }
+    function click() {
+      axios.put(`tg-game-production-8e6f.up.railway.app/?user_id=${userId()}`)
+      .catch(res=>console.log(res))
+    }
   return (
     <div className="App">
       <header className="App-header">

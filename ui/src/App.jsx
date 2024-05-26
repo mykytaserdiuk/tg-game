94a290189df5ea6e89461fd10f8a0e71617f35d3
Mykyta <hiban.com@gmail.com>
2024-05-27 00:15:32 +0200

    1

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,39 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-import { useSearchParams } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-
-const [searchParams] = useSearchParams();
-const [coin, setCoin] = useState(0);
-
-function userId(){
-  return searchParams.get("user_id")
-} 
-
-function getCoins() {
-  axios.get(`tg-game-production-8e6f.up.railway.app/?user_id=${userId()}`)
-  .then(response=> {
-    setCoin(response.data)
-  })
-  .catch(res=>console.log(res))
-}
-function click() {
-  axios.put(`tg-game-production-8e6f.up.railway.app/?user_id=${userId()}`)
-  .catch(res=>console.log(res))
-}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <p>
-        Coins: {coin}
-        ID: {userId}
-      </p>
-      <button onclick={click}>Add</button>
-    </div>
+    <Game>
+
+    </Game>
   );
 }
 

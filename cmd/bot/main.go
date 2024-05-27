6e10538f9ee5d40ac28118a731448cd44c04fef3
Mykyta <hiban.com@gmail.com>
2024-05-27 15:20:40 +0200

    1

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -18,8 +18,9 @@ var (
 	API = "7044707180:AAGMAqNArCMBo7Vr_7ykq89gtd9qOnWSHDg"
 )
 
+var gameURL = os.Getenv("front-end-url")
+
 func echo(b *gotgbot.Bot, ctx *ext.Context) error {
-	gameURL := os.Getenv("front-end-url")
 
 	if gameURL == "" {
 		_, _ = b.SendMessage(ctx.EffectiveChat.Id, "Error on the server. Please try again", nil)
